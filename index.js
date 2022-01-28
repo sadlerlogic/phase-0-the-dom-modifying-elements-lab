95e1bb179f5b6217c79b139c9696977e1aaee7e1
Christopher Sadler <sadlerlogic@gmail.com>
2022-01-27 20:40:52 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const say = "Hi"
+
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Christopher is the champion!";
+
+newHeader.className = "victory";
